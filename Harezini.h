9b7ea13afd79e6609e681839b6ea504ac8f472b5
Adocuss <da3son@gmail.com>
2019-12-02 13:33:46 -0600

    fixes compile issue?

--- Harezini.h
+++ Harezini.h
@@ -40,8 +40,10 @@
 	#include "./raygl/raygl.h"
 	#include "./raygl/raygldefs.h"
 #else
-	#define STB_IMAGE_IMPLEMENTATION
-	#include "stb_image.h"
+	#ifndef STB_IMAGE_IMPLEMENTATION
+		#define STB_IMAGE_IMPLEMENTATION
+		#include "stb_image.h"
+	#endif
 #endif
 
 // functions
