89564aaf19e0820459c7821af8eb2eff302b8618
Wilson <david.andrew.wilson@undcemlh112-14l.ad.und.edu>
2019-12-08 16:22:41 -0600

    Added moveCam3f, and added functions to Harezini.h

--- Harezini.h
+++ Harezini.h
@@ -27,16 +27,10 @@
 	const float BLACK[]	= {0.0, 0.0, 0.0, 1.0};
 	const float WHITE[]	= {1.0, 1.0, 1.0, 1.0};
 	const float GRAY[]	= {0.5, 0.5, 0.5, 1.0};
-	const float RED[]		= {1.0, 0.5, 0.5, 1.0};
+	const float RED[]	= {1.0, 0.5, 0.5, 1.0};
 	const float GREEN[]	= {0.0, 1.0, 0.0, 1.0};
 	const float BLUE[]	= {0.25, 0.5, 1.0, 1.0};
 #endif
-
-/* image loader
-#ifndef STB_IMAGE_IMPLEMENTATION
-	#define STB_IMAGE_IMPLEMENTATION
-	#include "stb_image.h"
-#endif*/
 
 // RAYGL
 #define RAYGL 0
@@ -52,10 +46,17 @@
 
 // functions
 void initialize();
-void myDisplay();
 void reshape(GLsizei width, GLsizei height);
-void specialKeyPressed(int key, int xMouse, int yMouse);
+void myDisplay();
+void moveCam3f (float xChange, float yChange, float zChange);
 void moveCamForward();
 void moveCamBackward();
-void keyboardKeyPressed(unsigned char key, int xMouse, int yMouse);
+void rotateCam(int direction, float degrees)
 void idleFunction();
+
+/*
+		REMOVE LATER
+*/
+
+void keyboardKeyPressed(unsigned char key, int xMouse, int yMouse);
+void specialKeyPressed(int key, int xMouse, int yMouse);
